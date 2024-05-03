e962100ce7410927d4695b745fcdff6ca77560dd
Denis Svetleishii <denissvetleishii@outlook.com>
2024-05-03 17:59:05 +0400

    feat:RSS-ECOMM-1_08 add appearing favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,4 +8,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"),
+        filename: "index.html",
+        favicon: path.join(__dirname, 'src', 'favicon.ico'),
+    }),
+  ],
 };
