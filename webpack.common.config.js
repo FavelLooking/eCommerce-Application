babdd157645cae92e97bf8f15e146293f3e4fee0
Denis Svetleishii <denissvetleishii@outlook.com>
2024-05-05 15:16:48 +0400

    refactor:RSS-ECOMM-1_08 separation rules for webpack  development and production mode

---
+++ webpack.common.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            favicon: path.join(__dirname, 'src', 'favicon.ico'),
+        }),
+        new CleanWebpackPlugin(),
+        new EslintPlugin({ extensions: ['ts'] }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|svg|jpeg|gif)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+            },
+        ],
+    },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,7 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const EslintPlugin = require('eslint-webpack-plugin')
-
-module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.ts'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-            filename: 'index.html',
-            favicon: path.join(__dirname, 'src', 'favicon.ico'),
-        }),
-        new CleanWebpackPlugin(),
-        new EslintPlugin({ extensions: ['ts'] }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(jpg|png|svg|jpeg|gif)$/,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.ts$/i,
-                use: 'ts-loader',
-            },
-        ],
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-}
+module.exports = (env, argv) => {
+    const mode = argv.mode || 'development';
+    if (mode === 'production') {
+        return require('./webpack.prod.config');
+    }
+    return require('./webpack.dev.config');
+};
