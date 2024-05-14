d17dadfc2b51e2f80bca063c2e5a6e82dc587236
Denis Svetleishii <denissvetleishii@outlook.com>
2024-05-14 20:35:04 +0400

    KAN-59-RSS-ECOMM-2_09 refactor: rename components directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import './style.scss';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import RegisterPage from './componets/registration_form/registration_form_render';
+import RegisterPage from './components/registration_form/registration_form_render';
 
 const container = document.body;
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
