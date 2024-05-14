d9ce340fde68bcc68b3bd1bcfe24671c8aa78fd4
Denis Svetleishii <denissvetleishii@outlook.com>
2024-05-14 19:42:10 +0400

    KAN-59-RSS-ECOMM-2_09 refactor: rename file registartion_form_render and fix typos

--- src/componets/registration_form/registration_form_render.tsx
+++ src/componets/registration_form/registration_form_render.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import EmailInput from './registration_form_inputs/email_input';
 import PasswordInput from './registration_form_inputs/password_input';
-import FirsNameInput from './registration_form_inputs/first_name_input';
+import FirstNameInput from './registration_form_inputs/first_name_input';
 import BirthDateInput from './registration_form_inputs/birth_date_input';
-import AdressField from './registration_form_inputs/adress_field';
+import AddressField from './registration_form_inputs/adress_field';
 import LastNameInput from './registration_form_inputs/last_name_input';
 
 function RegisterPage() {
@@ -13,10 +13,10 @@ function RegisterPage() {
     <form className="registration-form" onSubmit={handleRegister}>
       <EmailInput />
       <PasswordInput />
-      <FirsNameInput />
+      <FirstNameInput />
       <LastNameInput />
       <BirthDateInput />
-      <AdressField />
+      <AddressField />
       <button type="submit">Register</button>
     </form>
   );
