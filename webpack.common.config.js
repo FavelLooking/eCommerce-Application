5c68cd6b9bd809c86843e2426acafb1fbc1508fc
FavelLooking <intwestpol@gmail.com>
2024-05-15 22:43:48 +0400

    KAN-85-RSS-ECOMM-2_30 feat: add commerce tools setup

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: {
@@ -24,6 +25,7 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new EslintPlugin({ extensions: ['tsx'] }),
+    new Dotenv(),
   ],
   module: {
     rules: [
