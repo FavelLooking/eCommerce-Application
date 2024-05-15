96c512a447442f4be279af15f1b0a6235c81d0c7
Denis Svetleishii <denissvetleishii@outlook.com>
2024-05-15 19:42:09 +0400

    KAN-60-RSS-ECOMM-2_10 feat: add dynamic render error, when country change

--- src/components/registration_form/registration_form_inputs/adress_field_inputs/postal_code_input.tsx
+++ src/components/registration_form/registration_form_inputs/adress_field_inputs/postal_code_input.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useMemo, useState } from 'react';
 import InputStatus from '../../registration_form_interfaces';
 import CountryType from '../../registration_form_types';
 
@@ -9,19 +9,32 @@ function PostalCodeInput({
   const [inputValue, setInputValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const postalCode = event.target.value;
-    setInputValue(postalCode);
-
-    const postalCodeFormats = {
+  const postalCodeFormats = useMemo(
+    () => ({
       US: /^\d{5}$/,
       CA: /^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$/,
       RU: /^\d{6}$/,
       GE: /^\d{4}$/,
-    };
+    }),
+    []
+  );
+
+  useEffect(() => {
+    if (selectedCountry && inputValue !== '') {
+      const isValidSelectedCountry =
+        postalCodeFormats[selectedCountry as CountryType].test(inputValue);
+      setIsValid(isValidSelectedCountry);
+      onValidationChange(isValidSelectedCountry);
+    }
+  }, [selectedCountry, inputValue, onValidationChange, postalCodeFormats]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const postalCode = event.target.value;
+    setInputValue(postalCode);
 
     const isValidCountry =
       postalCodeFormats[selectedCountry as CountryType].test(postalCode);
+
     setIsValid(isValidCountry);
     onValidationChange(isValidCountry);
   };
@@ -33,7 +46,7 @@ function PostalCodeInput({
     >
       <p className="registration-input__postal-code-lable">postal code:</p>
       <input
-        id="postal-code-inpur"
+        id="postal-code-input"
         type="text"
         placeholder="postal-code"
         value={inputValue}
