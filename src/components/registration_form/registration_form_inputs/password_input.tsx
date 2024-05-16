33e7daa26297b806276534e183b4f8cc2d9e5f05
Denis Svetleishii <denissvetleishii@outlook.com>
2024-05-16 18:08:55 +0400

    KAN-60-RSS-ECOMM-2_10 fix: fix handleChange logic into PasswordInput

--- src/components/registration_form/registration_form_inputs/password_input.tsx
+++ src/components/registration_form/registration_form_inputs/password_input.tsx
@@ -1,5 +1,7 @@
 import React, { ChangeEvent, useState } from 'react';
 import InputStatus from '../registration_form_interfaces';
+import validationInput from '../registration_form_validation_regex';
+import { passwordPatternRegistration } from '../registration_form_regex';
 
 function PasswordInput({ onValidationChange }: InputStatus): JSX.Element {
   const [inputValue, setInputValue] = useState('');
@@ -9,11 +11,10 @@ function PasswordInput({ onValidationChange }: InputStatus): JSX.Element {
     const password: string = event.target.value;
     setInputValue(password);
 
-    const passwordRegex: RegExp =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-    const isValidPassword = passwordRegex.test(password);
-    setIsValid(isValidPassword);
-    onValidationChange(isValidPassword);
+    setIsValid(validationInput(passwordPatternRegistration.regex, password));
+    onValidationChange(
+      validationInput(passwordPatternRegistration.regex, password)
+    );
   };
 
   return (
@@ -31,10 +32,7 @@ function PasswordInput({ onValidationChange }: InputStatus): JSX.Element {
         style={{ borderColor: isValid ? 'initial' : 'red' }}
       />
       {!isValid && (
-        <div style={{ color: 'red' }}>
-          password must contains minimum 8 characters, at least 1 uppercase
-          letter, 1 lowercase letter, and 1 number
-        </div>
+        <div style={{ color: 'red' }}>{passwordPatternRegistration.error}</div>
       )}
     </label>
   );
