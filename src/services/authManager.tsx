152f49b026795c33bf4a275dfb4abece59695ad2
FavelLooking <118120216+FavelLooking@users.noreply.github.com>
2024-05-17 22:15:38 +0400

    KAN-87-RSS-ECOMM-2_32 feat: implement configuration and authentication client factory for Commerce Tools (#22)

---
+++ src/services/authManager.tsx
@@ -0,0 +1,58 @@
+import {
+  type HttpMiddlewareOptions,
+  type PasswordAuthMiddlewareOptions,
+  type AnonymousAuthMiddlewareOptions,
+} from '@commercetools/sdk-client-v2';
+import ConfigManager from './configManager';
+
+const { v4: uuidv4 } = require('uuid');
+
+class AuthManager {
+  private static config = ConfigManager.createConfig();
+
+  private static anonymousId = uuidv4();
+
+  static getHttpMiddlewareOptions(): HttpMiddlewareOptions {
+    return {
+      host: this.config.apiBaseUrl,
+      fetch,
+    };
+  }
+
+  static provideOptionsForPasswordFlow(
+    username: string,
+    password: string
+  ): PasswordAuthMiddlewareOptions {
+    return {
+      host: this.config.authBaseUrl,
+      projectKey: this.config.projectKey,
+      credentials: {
+        clientId: this.config.clientId,
+        clientSecret: this.config.clientSecret,
+        user: { username, password },
+      },
+      scopes: this.config.scopes,
+      fetch,
+    };
+  }
+
+  static getOptionsForAnonymousFlow(): AnonymousAuthMiddlewareOptions {
+    return {
+      host: this.config.authBaseUrl,
+      projectKey: this.config.projectKey,
+      credentials: {
+        clientId: this.config.clientId,
+        clientSecret: this.config.clientSecret,
+        anonymousId: this.anonymousId,
+      },
+      scopes: this.config.scopes,
+      fetch,
+    };
+  }
+
+  static getProjectKey(): string {
+    return this.config.projectKey;
+  }
+}
+
+export default AuthManager;
---
+++ src/types/clientFactory.tsx
@@ -0,0 +1,1 @@
+export type FlowType = 'password' | 'anonymous';
