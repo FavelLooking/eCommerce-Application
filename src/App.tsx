44f57cbe4309e423cebdd05b43c29abd4a1723fc
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-05-17 19:26:10 +0400

    KAN-84 feat: add routing (#19)
    * KAN-84 feat: add routing
    
    * KAN-84 fix: change path for 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './components/login/login_page';
 import RegisterPage from './components/registration_form/registration_form_render';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>Main Page</h1>
+      </div>
+    ),
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: 'register',
+    element: <RegisterPage />,
+  },
+  {
+    path: 'not-found',
+    element: (
+      <div>
+        <h1>404 Page Not Found</h1>
+      </div>
+    ),
+  },
+]);
+
 export default function App() {
-  return <RegisterPage />;
+  return <RouterProvider router={router} />;
 }
