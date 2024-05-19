a628fbf55e03c4c4ee95a27a3666fc5cf58296bb
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-05-19 16:10:54 +0400

    KAN-82 fix: move tests into separate subgroups

--- src/App.test.ts
+++ src/App.test.ts
@@ -20,70 +20,79 @@ describe('test validation on login page', () => {
   const numberError = loginRegex.textNumberPattern.error;
   const symbolError = loginRegex.textSymbolPattern.error;
 
-  test('should validate email', () => {
-    // valid email
-    expect(validateInput(emailValidationRegex, 'test@gmail.com')).toBe(true);
-    // whitespace before email
-    expect(validateInput(emailValidationRegex, ' test@gmail.com')).toBe(
-      emailError
-    );
-    // whitespace in email
-    expect(validateInput(emailValidationRegex, 'test @gmail.com')).toBe(
-      emailError
-    );
-    // whitespace after email
-    expect(validateInput(emailValidationRegex, 'test@gmail.com ')).toBe(
-      emailError
-    );
-    // incorrect domain part
-    expect(validateInput(emailValidationRegex, 'test@gmail')).toBe(emailError);
-    // incorrect domain part
-    expect(validateInput(emailValidationRegex, 'test@gmail.')).toBe(emailError);
-    // incorrect domain part
-    expect(validateInput(emailValidationRegex, 'test@gmail.c')).toBe(
-      emailError
-    );
-    // incorrect domain part
-    expect(validateInput(emailValidationRegex, 'test@.com')).toBe(emailError);
-    // no prefix
-    expect(validateInput(emailValidationRegex, '@.com')).toBe(emailError);
-    // no @ character
-    expect(validateInput(emailValidationRegex, 'test.com')).toBe(emailError);
-    // two @ characters
-    expect(validateInput(emailValidationRegex, 'te@st@.com')).toBe(emailError);
+  describe('email', () => {
+    test('should validate email', () => {
+      expect(validateInput(emailValidationRegex, 'test@gmail.com')).toBe(true);
+    });
+    test('should throw an error if incorrect prefix-part', () => {
+      expect(validateInput(emailValidationRegex, ' test@gmail.com')).toBe(
+        emailError
+      );
+      expect(validateInput(emailValidationRegex, 'test @gmail.com')).toBe(
+        emailError
+      );
+      expect(validateInput(emailValidationRegex, 'test@gmail.com ')).toBe(
+        emailError
+      );
+    });
+    test('should throw an error if incorrect domain part', () => {
+      expect(validateInput(emailValidationRegex, 'test@gmail')).toBe(
+        emailError
+      );
+      expect(validateInput(emailValidationRegex, 'test@gmail.')).toBe(
+        emailError
+      );
+      expect(validateInput(emailValidationRegex, 'test@gmail.c')).toBe(
+        emailError
+      );
+      expect(validateInput(emailValidationRegex, 'test@.com')).toBe(emailError);
+      expect(validateInput(emailValidationRegex, '@.com')).toBe(emailError);
+      expect(validateInput(emailValidationRegex, 'test.com')).toBe(emailError);
+      expect(validateInput(emailValidationRegex, 'te@st@.com')).toBe(
+        emailError
+      );
+    });
   });
-  test('should validate password', () => {
-    // valid password
-    expect(validateInput(passwordValidationRegex, 'Qwerty123!')).toBe(true);
-    // whitespace before password
-    expect(validateInput(passwordValidationRegex, ' Qwerty123!')).toBe(
-      spacesError
-    );
-    // whitespace in password
-    expect(validateInput(passwordValidationRegex, ' Qwert y123!')).toBe(
-      spacesError
-    );
-    // whitespace after password
-    expect(validateInput(passwordValidationRegex, ' Qwerty123! ')).toBe(
-      spacesError
-    );
-    // wrong length of password
-    expect(validateInput(passwordValidationRegex, 'Qwerty')).toBe(lengthError);
-    // no lowercase letter in password
-    expect(validateInput(passwordValidationRegex, 'QQQQQ123!')).toBe(
-      lowercaseError
-    );
-    // no uppercase letter in password
-    expect(validateInput(passwordValidationRegex, 'qqqqqq123!')).toBe(
-      uppercaseError
-    );
-    // no number in password
-    expect(validateInput(passwordValidationRegex, 'Qwerty!!!!')).toBe(
-      numberError
-    );
-    // no special symbol in password
-    expect(validateInput(passwordValidationRegex, 'Qwerty1234')).toBe(
-      symbolError
-    );
+
+  describe('password', () => {
+    test('should validate password', () => {
+      expect(validateInput(passwordValidationRegex, 'Qwerty123!')).toBe(true);
+    });
+    test('should throw an error if password contains whitespaces', () => {
+      expect(validateInput(passwordValidationRegex, ' Qwerty123!')).toBe(
+        spacesError
+      );
+      expect(validateInput(passwordValidationRegex, ' Qwert y123!')).toBe(
+        spacesError
+      );
+      expect(validateInput(passwordValidationRegex, ' Qwerty123! ')).toBe(
+        spacesError
+      );
+    });
+    test('should throw an error if password length is incorrect', () => {
+      expect(validateInput(passwordValidationRegex, 'Qwerty')).toBe(
+        lengthError
+      );
+    });
+    test('should throw an error if password not contains at least one lowercase english letter', () => {
+      expect(validateInput(passwordValidationRegex, 'QQQQQ123!')).toBe(
+        lowercaseError
+      );
+    });
+    test('should throw an error if password not contains at least one uppercase english letter', () => {
+      expect(validateInput(passwordValidationRegex, 'qqqqqq123!')).toBe(
+        uppercaseError
+      );
+    });
+    test('should throw an error if password not contains at least one number', () => {
+      expect(validateInput(passwordValidationRegex, 'Qwerty!!!!')).toBe(
+        numberError
+      );
+    });
+    test('should throw an error if password not contains at least one special symbol !@#$%^&*', () => {
+      expect(validateInput(passwordValidationRegex, 'Qwerty1234')).toBe(
+        symbolError
+      );
+    });
   });
 });
