854900a2b23d4d1b83b36bade4574bec497a349c
Denis Svetleishii <denissvetleishii@outlook.com>
2024-05-27 01:35:13 +0400

    KAN-111-RSS-ECOMM-3_09 refactor: change path of types detailed product page

--- src/interfaces/detailed_product_page_interfaces/detailed_product_page_interfaces.tsx
+++
@@ -1,9 +0,0 @@
-export interface ImageDetailedPage {
-  url: string;
-}
-
-export interface ProductDataDetailedPage {
-  productName: string;
-  productDescription?: string;
-  productImages?: ImageDetailedPage[];
-}
