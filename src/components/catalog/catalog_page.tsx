a6f49ce1867d60c4812b55fe688ee204944314ef
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-05-30 15:06:44 +0400

    KAN-110 feat: add catalog item on separate component

--- src/components/catalog/catalog_page.tsx
+++ src/components/catalog/catalog_page.tsx
@@ -2,63 +2,23 @@ import React from 'react';
 import { Navigate, useLoaderData } from 'react-router-dom';
 import { Product } from '@commercetools/platform-sdk';
 import './catalog.scss';
-import {
-  getProductDescription,
-  getProductImage,
-  getProductName,
-  getProductPrice,
-  getProducts,
-} from '../../services/productService';
+import { getProducts } from '../../services/productService';
 import Breadcrumb from './breadcrumb';
+import CatalogItem from './catalog_item';
 
 export const catalogLoader = async () => getProducts();
 
 export function CatalogPage() {
   const data: Product[] = useLoaderData() as Product[];
 
-  const getPrice = (product: Product): JSX.Element => {
-    const [originalPrice, discountPrice] = getProductPrice(product);
-
-    if (!originalPrice) {
-      return <div className="catalog-item-price">SOLD OUT</div>;
-    }
-
-    return discountPrice ? (
-      <div className="catalog-item-price">
-        <span className="catalog-price">{discountPrice}</span>
-        <span className="catalog-price catalog-discounted">
-          {originalPrice}
-        </span>
-      </div>
-    ) : (
-      <div className="catalog-item-price">
-        <span className="catalog-price">{originalPrice}</span>
-      </div>
-    );
-  };
-
   return data.length ? (
     <div className="catalog_wrapper">
       <Breadcrumb />
       <div className="catalog_flex">
-        {data.map((product) => (
-          <div className="catalog-item" key={product.id}>
-            <img
-              src={getProductImage(product)}
-              alt="product"
-              className="catalog-item-image"
-            />
-            <div className="catalog-item-info">
-              <span className="catalog-item-name">
-                {getProductName(product)}
-              </span>
-              <span className="catalog-item-description">
-                {getProductDescription(product)}
-              </span>
-              {getPrice(product)}
-            </div>
-            <input type="button" className="catalog-add" />
-          </div>
+        {data.map((item) => (
+          <li key={item.id}>
+            <CatalogItem product={item} />
+          </li>
         ))}
       </div>
     </div>
