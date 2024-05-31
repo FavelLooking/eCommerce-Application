73cde2c741264063149cb4c6c0e76743516526c0
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-05-31 23:34:31 +0400

    Kan 106 rss ecomm 3 04 implement sorting functionality for product list using commercetools api (#65)
    * KAN-106 fewat: add sort, search, and filter buttons
    
    * KAN-106 fewat: add sort by name ascending
    
    * KAN-106 feat: add sort by name and price

--- src/components/catalog/catalog_page.tsx
+++ src/components/catalog/catalog_page.tsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ProductProjection } from '@commercetools/platform-sdk';
 import './catalog.scss';
-import { getProducts } from '../../services/productService';
+import { getProducts, sortProducts } from '../../services/productService';
 import Breadcrumb from './breadcrumb';
 import CatalogItem from './catalog_item';
+import { SortingTypes } from '../../types';
 
 export default function CatalogPage() {
   const location = useLocation();
   const navigate = useNavigate();
   const [data, setData] = useState<ProductProjection[]>();
+  const [isSort, setSort] = useState(false);
 
   useEffect(() => {
     getProducts(location.pathname).then((value: ProductProjection[]) => {
@@ -18,9 +20,58 @@ export default function CatalogPage() {
     });
   }, [location, navigate]);
 
+  const sort = async (sortingType: string) => {
+    setSort(false);
+    sortProducts(location.pathname, sortingType).then(
+      (value: ProductProjection[]) => setData(value)
+    );
+  };
+
   return (
     <div className="catalog_wrapper">
-      <Breadcrumb />
+      <div className="some-catalog-div-with-breadcrumb-and-utils">
+        <Breadcrumb />
+        <div className="catalog-utils">
+          <input
+            type="button"
+            className="catalog-button catalog-toogle-search"
+          />
+          <input
+            type="button"
+            className="catalog-button catalog-toogle-filter"
+          />
+          <input
+            type="button"
+            className="catalog-button catalog-toogle-sort"
+            onClick={() => setSort(!isSort)}
+          />
+        </div>
+      </div>
+      <div
+        className="catalog-sorting"
+        style={{ display: `${isSort ? '' : 'none'}` }}
+      >
+        <input
+          type="button"
+          value="name ↑"
+          onClick={() => sort(SortingTypes.NAMEASC)}
+        />
+        <input
+          type="button"
+          value="name ↓"
+          onClick={() => sort(SortingTypes.NAMEDESC)}
+        />
+        <input
+          type="button"
+          value="price ↑"
+          onClick={() => sort(SortingTypes.PRICEASC)}
+        />
+        <input
+          type="button"
+          value="price ↓"
+          onClick={() => sort(SortingTypes.PRICEDESC)}
+        />
+      </div>
       <div className="catalog_flex">
         {data?.map((item) => (
           <li key={item.id}>
