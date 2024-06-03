3c3f9af4e7128a3e6ad5a5635dbb1c8189207e56
FavelLooking <118120216+FavelLooking@users.noreply.github.com>
2024-06-04 02:59:41 +0400

    KAN-119-RSS-ECOMM-3_17 feat: implement password change in user profile page (#68)
    * KAN-119-RSS-ECOMM-3_17 feat: add routing and form for change password feature
    
    * KAN-119-RSS-ECOMM-3_17 feat: implement re-authentication
    
    * KAN-119-RSS-ECOMM-3_17 fix: fix routing and cancel button behavior
    
    * KAN-119-RSS-ECOMM-3_17 refactor:replace reconnect method to AuthService class

--- src/services/customerService.tsx
+++ src/services/customerService.tsx
@@ -1,9 +1,39 @@
 import ClientFactory from './clientFactory';
+import AuthService from './authService';
 
 class CustomerService {
   static getCustomersDetails = async () => {
     const apiRoot = ClientFactory.createApiRoot(ClientFactory.flowType);
     return apiRoot.me().get().execute();
+  };
+
+  static changePassword = async (
+    currentPassword: string,
+    newPassword: string
+  ) => {
+    try {
+      const customerDetails = await CustomerService.getCustomersDetails();
+      const { body } = customerDetails;
+      const { version, email } = body;
+
+      const apiRoot = ClientFactory.createApiRoot(ClientFactory.flowType);
+      await apiRoot
+        .me()
+        .password()
+        .post({
+          body: {
+            version,
+            currentPassword,
+            newPassword,
+          },
+        })
+        .execute();
+      return email;
+    } catch (error: unknown) {
+      const errorMessage = (error as Error).message;
+      AuthService.saveToLocalStorage('ErrorMessage', errorMessage);
+    }
+    return undefined;
   };
 }
 
