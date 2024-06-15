4e08dc1dece5149dbd72a6ad823ca68118f75887
Denis Svetleishii <denissvetleishii@outlook.com>
2024-06-15 17:26:58 +0400

    KAN-139-RSS-ECOMM-4_01 fix: reduce toasty appearing when product add in cart

--- src/utils/cart_utils/create_cart.tsx
+++ src/utils/cart_utils/create_cart.tsx
@@ -6,6 +6,7 @@ export default async function CreateCart(item: string) {
   const showToast = (text: string) => {
     Toastify({
       text,
+      duration: 1000,
       className: 'info',
       style: {
         background: 'linear-gradient(to right, #00b09b, #96c93d)',
