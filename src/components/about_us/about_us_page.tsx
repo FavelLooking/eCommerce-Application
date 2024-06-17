4af8d793740a86e6d3880bb64831f07dc2511d9d
FavelLooking <118120216+FavelLooking@users.noreply.github.com>
2024-06-17 21:30:49 +0400

    KAN-154-RSS-ECOMM-4_20 feat: add school logo and description (#97)

--- src/components/about_us/about_us_page.tsx
+++ src/components/about_us/about_us_page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './about.scss';
 import { teamDescription } from '../../utils/about_constants';
 import TeamMember from './team_member';
+import SchoolComponent from './school_component';
 
 export default function AboutUsPage() {
   return (
     <div className="about">
       <div className="team-description">{teamDescription}</div>
       <TeamMember />
+      <SchoolComponent />
     </div>
   );
 }
